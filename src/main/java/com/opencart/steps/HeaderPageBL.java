2b0487c45c2887c0a1962ce8663726a892bb49d4
yurii <yuri.krynytsky@gmail.com>
2020-11-07 16:15:20 +0200

    Krynytsky.Separate business logic from class that describes the page

---
+++ src/main/java/com/opencart/steps/HeaderPageBL.java
@@ -0,0 +1,23 @@
+package com.opencart.steps;
+
+import com.opencart.pages.HeaderPage;
+
+
+public class HeaderPageBL {
+
+    private HeaderPage headerPage;
+
+    public HeaderPageBL() {
+        headerPage = new HeaderPage();
+    }
+
+    public HeaderPageBL clickOnMyAccountButton() {
+        headerPage.getMyAccountButton().click();
+        return this;
+    }
+
+    public HeaderPageBL clickOnRegisterButton() {
+        headerPage.getRegisterButton().click();
+        return this;
+    }
+}
