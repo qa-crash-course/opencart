3e59abb21af9de029f37bf0583838c15cc6ddc5d
yurii <yuri.krynytsky@gmail.com>
2020-11-07 11:27:33 +0200

    Krynytskyi. Create Driver Repository Class for connection to our web Browsers

---
+++ src/main/java/com/opencart/driver/DriverRepository.java
@@ -0,0 +1,32 @@
+package com.opencart.driver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Class where we connect the driver using selenium.WebDriver dependency
+ */
+
+public class DriverRepository {
+
+    public final static ThreadLocal<WebDriver> DRIVERS = new ThreadLocal<>();
+    public static WebDriver webDriver;
+
+    private DriverRepository() {
+    }
+
+    public static void downloadWebDriver() {
+        WebDriverManager.firefoxdriver().setup();
+    }
+
+    public static void instanceWebBrowser(){
+        webDriver = new FirefoxDriver();
+        DRIVERS.set(webDriver);
+    }
+    public static void closeBrowser(){
+        webDriver.quit();
+    }
+
+}
+
