f6e0052d358b65a1d2d76319cac1b4182d14ba33
yurii <yuri.krynytsky@gmail.com>
2020-11-08 18:42:25 +0200

    Krynytsky. Fix mistake in the (xpath = (".//*[@id='content]/h1")) to (xpath = ".//*[@id='content']/h1")

--- src/main/java/com/opencart/pages/SuccessRegisterPage.java
+++ src/main/java/com/opencart/pages/SuccessRegisterPage.java
@@ -3,11 +3,13 @@ package com.opencart.pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class SuccessRegisterPage extends BasePage {
-    @FindBy(xpath = (".//*[@id='content]/h1"))
-    private WebElement succesTitle;
 
-    public WebElement getSuccesTitle() {
-        return succesTitle;
+public class SuccessRegisterPage extends BasePage {
+    @FindBy(xpath = ".//*[@id='content']/h1")
+    private WebElement successTitle;
+
+    public WebElement getSuccessTitle() {
+        return successTitle;
     }
+
 }
