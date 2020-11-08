42c081078b403bde2909d82397cba3aca1a274cc
yurii <yuri.krynytsky@gmail.com>
2020-11-08 19:34:18 +0200

    Krynytsky. Implement builder without the lombok annotation

--- src/main/java/com/opencart/datamodel/RegisterModel.java
+++ src/main/java/com/opencart/datamodel/RegisterModel.java
@@ -4,7 +4,6 @@ import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
 public class RegisterModel {
     private String firstName;
     private String lastName;
@@ -12,4 +11,47 @@ public class RegisterModel {
     private String telephone;
     private String password;
     private String passwordConfirm;
+
+    RegisterModel() {
+    }
+
+    public static RegisterModel.Builder getBuilder() {
+        return new RegisterModel().new Builder();
+    }
+
+    public class Builder {
+        public RegisterModel.Builder firstName(String value) {
+            firstName = value;
+            return this;
+        }
+
+        public RegisterModel.Builder lastName(String value) {
+            lastName = value;
+            return this;
+        }
+
+        public RegisterModel.Builder email(String value) {
+            email = value;
+            return this;
+        }
+
+        public RegisterModel.Builder telephone(String value) {
+            telephone = value;
+            return this;
+        }
+
+        public RegisterModel.Builder password(String value) {
+            password = value;
+            return this;
+        }
+
+        public RegisterModel.Builder passwordConfirm(String value) {
+            passwordConfirm = value;
+            return this;
+        }
+
+        public RegisterModel build() {
+            return RegisterModel.this;
+        }
+    }
 }
