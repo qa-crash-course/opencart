3aee447ad0f4f1b9e489620ef1e5fe42c46eb1ff
yurii <yuri.krynytsky@gmail.com>
2020-11-10 13:35:51 +0200

    Krynytsky.#7 Describe SearchProduct elements. Add SearchPhrases enum as a template of our search criteria

---
+++ src/main/java/com/opencart/enums/SearchPhrases.java
@@ -0,0 +1,13 @@
+package com.opencart.enums;
+
+public enum SearchPhrases {
+
+    MAC("mac");
+
+    private final String value;
+
+    SearchPhrases(String value) {
+        this.value = value;
+
+    }
+}
