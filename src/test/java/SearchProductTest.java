2b48fbfa699a231a4e6c494039b00c9c9a442fe3
yurii <yuri.krynytsky@gmail.com>
2020-11-11 17:26:28 +0200

    Krynytsky.#7 Additional validation of search

--- src/test/java/SearchProductTest.java
+++ src/test/java/SearchProductTest.java
@@ -3,16 +3,16 @@ import com.opencart.steps.MainPageBL;
 import com.opencart.steps.SearchProductPageBL;
 import org.testng.annotations.Test;
 
+import static com.opencart.enums.SearchPhrases.MAC;
 import static com.opencart.enums.URLs.BASE_URL;
 
 public class SearchProductTest extends BaseTest {
     @Test
     public void searchProduct() {
-
         new Navigation().navigateToUrl(BASE_URL.getValue());
         MainPageBL mainPageBL = new MainPageBL();
         SearchProductPageBL searchProductPageBL = mainPageBL.getSearchPageBL()
-                .makeSearch();
-        searchProductPageBL.verifySearchResult();
+                .makeSearch(MAC.getValue());
+        searchProductPageBL.verifySearchResult(MAC.getValue());
     }
 }
