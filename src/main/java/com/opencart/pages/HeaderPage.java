60b1563d62f409a9e41421c29a389ef17ba09d4d
TarasHrysiukk <64036257+TarasHrysiukk@users.noreply.github.com>
2020-11-15 19:37:32 +0200

    Добавив  кнопку логін

--- src/main/java/com/opencart/pages/HeaderPage.java
+++ src/main/java/com/opencart/pages/HeaderPage.java
@@ -2,6 +2,8 @@ package com.opencart.pages;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class HeaderPage extends BasePage {
 
@@ -11,11 +13,20 @@ public class HeaderPage extends BasePage {
     @FindBy(xpath = ".//*[contains(@href,'register')]")
     private WebElement registerButton;
 
+    @FindBy(xpath = ".//*[contains(@href,'Login')]" )
+    private WebElement loginButton;
+
     public WebElement getMyAccountButton() {
+        wait.until(ExpectedConditions.visibilityOf(myAccountButton));
         return myAccountButton;
     }
 
     public WebElement getRegisterButton() {
         return registerButton;
+    }
+
+    public WebElement getLoginButton(){
+        wait.until(ExpectedConditions.visibilityOf(loginButton));
+        return loginButton;
     }
 }
