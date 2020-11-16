a7ad26689b184a8b87340243d016751621405bba
yurii <yuri.krynytsky@gmail.com>
2020-11-16 10:20:31 +0200

    Krynytsky.#7 Helper for managing localhost urls

--- src/main/java/com/opencart/enums/URLs.java
+++ src/main/java/com/opencart/enums/URLs.java
@@ -1,9 +1,15 @@
 package com.opencart.enums;
 
 public enum URLs {
-    BASE_URL("http://localhost/upload/"),
+    BASE_URL(TeamLocalURLs.KRYNYTSKY_BASE_URL),
+    ADMIN_URL(TeamLocalURLs.KRYNYTSKY_ADMIN_URL);
 
-    ADMIN_URL("http://localhost/upload/admin");
+    private static class TeamLocalURLs
+    {
+        private static final String KRYNYTSKY_BASE_URL = "http://localhost/upload";
+        private static final String KRYNYTSKY_ADMIN_URL = "http://localhost/upload/admin";
+        private static final String HRYSIUK_BASE_URL = "http://localhost/opencart/upload/";
+    }
 
     private final String value;
 
